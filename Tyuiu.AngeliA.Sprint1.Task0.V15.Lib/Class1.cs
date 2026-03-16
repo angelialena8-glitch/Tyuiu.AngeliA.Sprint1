dab092d66612f2e6cc6df1d783f37cdf35283568
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-03-16 20:36:24 +0500

    Add project files.

---
+++ Tyuiu.AngeliA.Sprint1.Task0.V15.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.AngeliA.Sprint1.Task0.V15.Lib
+{
+    public class Class1 : ISprint1Task0V15
+    {
+        public double Calculate()
+        {
+            return 20 - (2 * 2 - 8);
+        }
+    }
+}
