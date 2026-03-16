007e7df94f7bc1f10c7a3ceebf0029d9cc5becb0
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-03-16 20:40:42 +0500

    Task0

---
+++ Tyuiu.AngeliA.Sprint1.Task0.V15/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.AngeliA.Sprint1.Task0.V15.Lib;
+namespace Tyuiu.AngeliA.Sprint1.Task0.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
