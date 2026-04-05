ce150a0489b94722cb7056f8159ae705d9590518
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-05 13:38:36 +0500

    Task2

---
+++ Tyuiu.AngeliA.Sprint1.Task2.V7.Lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.AngeliA.Sprint1.Task2.V7.Lib
+{
+    public class Class1 : ISprint1Task2V7
+    {
+        public double CalculateSquareCircle(int value)
+        {
+            double a = value * value * 3.1415926;
+            a = Math.Round(a, 3);
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.AngeliA.Sprint1.Task2.V7/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.AngeliA.Sprint1.Task2.V7.Lib;
+namespace Tyuiu.AngeliA.Sprint1.Task2.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
