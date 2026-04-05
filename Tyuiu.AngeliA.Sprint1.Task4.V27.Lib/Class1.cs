20f0202eaa2da3a7d42f9b5f8c0c8efd56d9232f
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-05 13:54:28 +0500

    Task4

---
+++ Tyuiu.AngeliA.Sprint1.Task4.V27.Lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.AngeliA.Sprint1.Task4.V27.Lib
+{
+    public class Class1 : ISprint1Task4V27
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = (1 + Math.Sin(x * Math.PI)) / (x - Math.Sqrt(Math.Abs(y)));
+            res = Math.Round(res, 3);
+            return res;
+        }
+    }
+}
